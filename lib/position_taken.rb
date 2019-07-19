945b81e40a124ecb8c2b0a561484872233fff376
Maddy Austin <maddy.austin@yahoo.co.uk>
2019-07-19 15:38:12 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " or board[index] == "" or board[index] == nil
     return false
-  elsif board[index] == "X" || "O"
+  elsif board[index] == "X" or board[index] == "O"
     return true
   else
     return nil
