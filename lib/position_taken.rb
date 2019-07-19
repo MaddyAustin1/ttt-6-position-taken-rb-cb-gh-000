5df813d033eb5a9ff052c55d7931c8313172ef02
Maddy Austin <maddy.austin@yahoo.co.uk>
2019-07-19 15:29:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " " or "" or nil
+    puts false
+  elsif board[index] == "X" or "O"
+    puts true
+  end
+
+end
